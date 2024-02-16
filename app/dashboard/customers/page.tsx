767df1cc974865e9c28f075b07a837957f6525f0
Loveleen Singh <loveleen.js@zestgeek.com>
2024-02-16 22:39:00 +0530

    commented font

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>Customer Pagee</div>;
+};
+
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+import { inter } from './ui/fonts';
+import '@/app/ui/global.css';
+import setupLocatorUI from '@locator/runtime';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  if (process.env.NODE_ENV === 'development') {
+    setupLocatorUI();
+  }
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
